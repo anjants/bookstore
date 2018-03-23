f44f7c32cc66391355055e8612a339d95ec22749
Anjan Tumkur <anjants@gmail.com>
2018-03-23 11:05:07 +0530

    Add storeapp, view, templates and URL

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+   # url(r'^blog/', include('blog.urls')),
+    url(r'^store/',include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
