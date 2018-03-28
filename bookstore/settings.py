2f34b4b9ac6d655ab03236a155cf6795d88ff003
Anjan Tumkur <anjants@gmail.com>
2018-03-28 17:59:10 +0530

    Add login logout test session variables

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -106,4 +106,16 @@ STATIC_URL = '/static/'
 # Registration variables
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = "/store/"
+
+#Email Settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'anjants@gmail.com'
+EMAIL_HOST_PASSWORD = 'hong@2512'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'bookstre@mysteryboks.com'
+
 
