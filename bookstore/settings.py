6b02a8d35b1bfc7165248b0ec5decec1f98d705e
Anjan Tumkur <anjants@gmail.com>
2018-04-19 17:11:13 +0530

    Refactored registration templates and aded compress feature

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'registration',
     'bootstrap3',
     'bootstrap_themes',
+    'compressor',
     'store',
 
 )
@@ -113,7 +114,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS=(
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 # Registration variables
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
